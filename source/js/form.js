dcc79672b3733ce00f97fca191af1d71f632aab4
SamoylenkVi <SamoylenkVi@users.noreply.github.com>
2021-07-06 00:04:59 +0300

    Добавляет маску для input tel

--- source/js/form.js
+++ source/js/form.js
@@ -1,6 +1,9 @@
 'use strict';
+
+const PHONE_CODE = '+7(';
+
 const regexPhoneSubmit = /^(\+7|8)?[\s\-]?\(?[0-9]{3}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/;
-const regexPhoneInput = /[^+-\d\s/(/)]/gi;
+const regexPhoneInput = new RegExp(/\+?7?\(?([0-9]{0,3})\)?([0-9]{0,3})\-?([0-9]{0,2})\-?([0-9]{0,2})/, 'g');
 const mainPhone = document.querySelector('#phone');
 const mainName = document.querySelector('#name');
 const mainForm = document.querySelector('section.form form');
@@ -8,11 +11,39 @@ const popupPhone = document.querySelector('#popup-phone');
 const popupName = document.querySelector('#popup-name');
 const popupForm = document.querySelector('section.popup form');
 
+const focusPhoneHandler = (evt) => {
+  if (evt.target.value.length === 0) {
+    evt.target.value = PHONE_CODE;
+  }
+};
+
+const maskPhoneHandler = (evt) => {
+
+  const [, group1, group2, group3, group4] = Array.from(evt.target.value.matchAll(regexPhoneInput))[0];
+
+  evt.target.value = PHONE_CODE;
+
+  if (group1) {
+    evt.target.value += group1;
+  }
+
+  if (group2) {
+    evt.target.value += `)${group2}`;
+  }
+
+  if (group3) {
+    evt.target.value += `-${group3}`;
+  }
+
+  if (group4) {
+    evt.target.value += `-${group4}`;
+  }
+};
+
 const validate = (inputPhone, inputName, form) => {
   if (inputPhone) {
-    inputPhone.addEventListener('input', () => {
-      inputPhone.value = inputPhone.value.replaceAll(regexPhoneInput, '');
-    });
+    inputPhone.addEventListener('focus', focusPhoneHandler);
+    inputPhone.addEventListener('input', maskPhoneHandler);
   }
   if (form && inputName && inputPhone) {
     form.setAttribute('novalidate', 'novalidate');
